ed0eb7216064c356f9210e7ce767f05aa2af19f0
CasCadeVR <girmilend@gmail.com>
2025-07-04 12:11:15 +0300

    Cleaned up main branch, leaving only README.md

--- Works.Repository.Contracts/IWorksWriteRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Works.Context.Contracts;
-
-namespace Works.Repository.Contracts;
-
-/// <summary>
-/// Репозиторий записи <see cref="Entities.Work"/>
-/// </summary>
-public interface IWorksWriteRepository : IDBWriter<Entities.Work> { }
